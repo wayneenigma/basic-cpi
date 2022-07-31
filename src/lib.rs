e835b09c1c1a30fa3543783fc63f186f3ec927ad
wayneenigma <enigmawayne90@gmail.com>
2022-07-31 16:21:47 +0545

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+mod basic {
+    use super::*;
+    pub fn init(_ctx: Context<Init>) -> Result<()> {
+        msg!("initialized");
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Init {}
